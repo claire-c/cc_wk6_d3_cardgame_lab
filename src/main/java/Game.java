7808950eb424509db9d81079ab72497ca990ddaa
Claire <cconnachan@gmail.com>
2018-04-11 15:13:38 +0100

    added game class, passed first set of tests

---
+++ src/main/java/Game.java
@@ -0,0 +1,19 @@
+public class Game {
+
+    private Player player1;
+    private Player player2;
+
+    public Game(Player player1, Player player2){
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public Player getPlayer1(){
+        return this.player1;
+    }
+
+    public Player getPlayer2(){
+        return this.player2;
+    }
+
+}
---
+++ src/test/java/GameTest.java
@@ -0,0 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameTest {
+
+    private Player player1;
+    private Player player2;
+    private Deck deck;
+    private Game game;
+
+    @Before
+    public void before(){
+        player1 = new Player("Claire");
+        player2 = new Player("Joseph");
+        deck = new Deck();
+        game = new Game(player1, player2);
+    }
+
+    @Test
+    public void canGetPlayer1(){
+        assertEquals("Claire", game.getPlayer1().getName());
+    }
+
+    @Test
+    public void canGetPlayer2(){
+        assertEquals("Joseph", game.getPlayer2().getName());
+    }
+
+
+}
